d1de5bc583b2be32c6681120cd6d60beecd983db
OriginalSin <saleks@scanex.ru>
2018-01-22 15:51:44 +0300

    add submodules shell

---
+++ addSubmodules.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git.exe submodule add   -- "git@github.com:ScanEx/es6-promise.git" "external/es6-promise"
+git.exe submodule add   -- "git@github.com:ScanEx/fetch.git" "external/fetch"
+git.exe submodule add   -- "git@github.com:ScanEx/Leaflet-GeoMixer1.2.git" "external/Leaflet-GeoMixer"
+git.exe submodule add   -- "git@github.com:ScanEx/gmxControls.git" "external/gmxControls"
+git.exe submodule add   -- "git@github.com:ScanEx/gmxDrawing.git" "external/gmxDrawing"
+git.exe submodule add -b 12  -- "git@github.com:ScanEx/Leaflet.gmxGrid.git" "external/Leaflet.gmxGrid"
+git.exe submodule add   -- "git@github.com:ScanEx/L.TileLayer.Mercator.git" "external/L.TileLayer.Mercator"
+git.exe submodule add   -- "git@github.com:ScanEx/Leaflet.imageTransform.git" "external/Leaflet.imageTransform"
+git.exe submodule add   -- "git@github.com:ScanEx/Leaflet.gmxBaseLayersManager.git" "external/Leaflet.gmxBaseLayersManager"
+git.exe submodule add   -- "git@github.com:ScanEx/Leaflet.contextmenu.git" "external/Leaflet.contextmenu"
+git.exe submodule add   -- "git@github.com:ScanEx/GMXVirtualTileLayer.git" "external/GMXVirtualTileLayer"
+git.exe submodule add   -- "git@github.com:ScanEx/Leaflet.GeometryUtil.git" "external/Leaflet.GeometryUtil"
